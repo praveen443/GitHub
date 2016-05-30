c218b1e9a9f82c171a53ffa3cf578a4f1c39a670
praveen varma <npraveen.443@gmail.com>
2016-05-30 13:27:12 +0530

    Updated the test case - verifyToolTipExample.

--- Desktop/eclipse/SeleniumProject/SampleProgram/src/TestCases/verifyToolTipExample.java
+++ Desktop/eclipse/SeleniumProject/SampleProgram/src/TestCases/verifyToolTipExample.java
@@ -2,6 +2,7 @@ package TestCases;
 
 import java.util.concurrent.TimeUnit;
 
+import org.eclipse.sisu.Priority;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -29,8 +30,29 @@ public class verifyToolTipExample {
 	  driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
   }
 
-  @Test
-  public void verifyToolTipText() {
+  /*
+   * @summary: By default, methods annotated by @Test are executed alphabetically. 
+   *   You need to use the ‘priority‘ parameter, if you want the methods to be executed in your order.
+   *   Parameters are keywords that modify the annotation’s function.
+   * @author: Praveen Namburi,@version: Created 30-05-2016.
+   */
+  
+  //prioritized to execute this method as first.
+  @Test(priority=1)
+  public void verifyToolTipText_Case1() {
+	  //Here the tooltip text is captured by using selenium getAttribute() method.
+	  WebElement eleToolTipText = driver.findElement(By.cssSelector("#header>h1 a"));
+	  String getToolTipText = eleToolTipText.getAttribute("title");
+	  TestReporter.log("Captured tooltip text is : " + getToolTipText);
+	  
+	  //verifying Captured tooltip text
+	  TestReporter.log("Verifying the Captured tooltip text.");
+	  Assert.assertEquals(getToolTipText, actualToolTipText);
+  }
+  
+  //prioritized to execute this method as second.
+  @Test(priority=2)
+  public void verifyToolTipText_Case2() {
 	  //Here the tooltip text is captured by using selenium getAttribute() method.
 	  WebElement eleToolTipText = driver.findElement(By.cssSelector("#header>h1 a"));
 	  String getToolTipText = eleToolTipText.getAttribute("title");
