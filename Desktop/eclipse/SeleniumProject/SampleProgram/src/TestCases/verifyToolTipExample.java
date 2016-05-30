9e15d17033580d20f78ca966ccaf21cd01b8e2fc
praveen varma <npraveen.443@gmail.com>
2016-05-30 16:10:53 +0530

    Updated the testcase - verifyToolTipExample with another @Test annotation method.

--- Desktop/eclipse/SeleniumProject/SampleProgram/src/TestCases/verifyToolTipExample.java
+++ Desktop/eclipse/SeleniumProject/SampleProgram/src/TestCases/verifyToolTipExample.java
@@ -2,12 +2,14 @@ package TestCases;
 
 import java.util.concurrent.TimeUnit;
 
-import org.eclipse.sisu.Priority;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
@@ -17,17 +19,16 @@ import Utils.TestReporter;
 public class verifyToolTipExample {
 
   public WebDriver driver;
-  public static final String appURL="http://www.seleniumhq.org/";
-  public static String actualToolTipText = "Return to Selenium home page";
+  public String seleniumURL = "http://docs.seleniumhq.org";
+  public String jQueryURL = "https://jqueryui.com/tooltip/";
+  public String actualToolTipText1 = "Return to Selenium home page";
+  public String actualToolTipText2 = "We ask for your age only for statistical purposes.";
   
   @BeforeMethod
   public void launchWebPage() {
 	  TestReporter.log("Launch the Firefox browser.");
 	  driver = new FirefoxDriver();
 	  driver.manage().window().maximize();
-	  TestReporter.log("Launch the Application.");
-	  driver.navigate().to(appURL);
-	  driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
   }
 
   /*
@@ -38,8 +39,12 @@ public class verifyToolTipExample {
    */
   
   //prioritized to execute this method as first.
-  @Test(priority=1)
+  @Test(priority=1,enabled=false)
   public void verifyToolTipText_Case1() {
+	  TestReporter.log("Launch the Application.");
+	  driver.navigate().to(seleniumURL);
+	  driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	  
 	  //Here the tooltip text is captured by using selenium getAttribute() method.
 	  WebElement eleToolTipText = driver.findElement(By.cssSelector("#header>h1 a"));
 	  String getToolTipText = eleToolTipText.getAttribute("title");
@@ -47,20 +52,41 @@ public class verifyToolTipExample {
 	  
 	  //verifying Captured tooltip text
 	  TestReporter.log("Verifying the Captured tooltip text.");
-	  Assert.assertEquals(getToolTipText, actualToolTipText);
+	  Assert.assertEquals(getToolTipText, actualToolTipText1);
   }
   
   //prioritized to execute this method as second.
-  @Test(priority=2,enabled=false)
+  @Test(priority=2)
   public void verifyToolTipText_Case2() {
-	  //Here the tooltip text is captured by using selenium getAttribute() method.
-	  WebElement eleToolTipText = driver.findElement(By.cssSelector("#header>h1 a"));
-	  String getToolTipText = eleToolTipText.getAttribute("title");
-	  TestReporter.log("Captured tooltip text is : " + getToolTipText);
-	  
-	  //verifying Captured tooltip text
-	  TestReporter.log("Verifying the Captured tooltip text.");
-	  Assert.assertEquals(getToolTipText, actualToolTipText);
+	  TestReporter.log("Launch the Application.");
+	  driver.navigate().to(jQueryURL);
+	  driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	
+	  try {
+			  //Here a frame exists, so we need to switch to it.
+			  WebDriverWait wait = new WebDriverWait(driver, 5);
+			  wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.cssSelector(".demo-frame")));
+			  
+			  //title="We ask for your age only for statistical purposes."
+			  //Textbox field, where we do mouse hover
+			  WebElement eleTextBox = driver.findElement(By.id("age"));
+			  
+			  //Use action class to mouse-hover on the text-box field 
+			  Actions action = new Actions(driver);
+			  action.moveToElement(eleTextBox).build().perform();
+			  WebElement eleToolTipText = driver.findElement(By.cssSelector(".ui-tooltip"));
+			  
+			  //Capture the tool tip text and assert it.
+			  String getToolTipText = eleToolTipText.getText();
+			  TestReporter.log("Captured tooltip text is : " + getToolTipText);
+			  
+			  //verifying Captured tooltip text
+			  TestReporter.log("Verifying the Captured tooltip text.");
+			  Assert.assertEquals(getToolTipText, actualToolTipText2);
+			  
+	  } catch (Exception e) {
+		  	  e.printStackTrace();
+	  }
   }
   
   @AfterMethod
