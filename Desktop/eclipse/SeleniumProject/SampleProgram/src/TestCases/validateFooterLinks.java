532b9ad7e1580507238aad472d8af19124dc94e0
praveen varma <npraveen.443@gmail.com>
2016-05-30 18:58:08 +0530

    Developed a new test in - validateFooterLinks.

---
+++ Desktop/eclipse/SeleniumProject/SampleProgram/src/TestCases/validateFooterLinks.java
@@ -0,0 +1,23 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class validateFooterLinks {
+  
+  //WebElement eleFooterLinks = driver.findElement(By.cssSelector("div[id$='block-28']>div>div>div>ul>li a"));  
+  //String appURL = "https://www.seleniumeasy.com/selenium-tutorials/css-selectors-tutorial-for-selenium-with-examples";
+  @Test
+  public void f() {
+  }
+  
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+}
