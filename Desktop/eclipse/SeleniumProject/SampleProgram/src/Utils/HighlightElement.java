c28f65eca042f84864f65903670c01b3cfe828a2
praveen varma <npraveen.443@gmail.com>
2016-06-01 16:47:02 +0530

    Updated the Utils class - HighLightElement.

--- Desktop/eclipse/SeleniumProject/SampleProgram/src/Utils/HighlightElement.java
+++ Desktop/eclipse/SeleniumProject/SampleProgram/src/Utils/HighlightElement.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebElement;
 
 public class HighlightElement {
 	//public static WebDriver driver;
-
 	public static void highlight(WebDriver driver,WebElement element){
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("arguments[0].setAttribute('style', 'background: yellow; border: 2px solid red;');", element);
