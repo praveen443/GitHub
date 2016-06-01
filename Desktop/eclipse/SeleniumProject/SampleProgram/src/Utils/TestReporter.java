702ca4d1e6739c1dfe428fa851dedf229aa881ab
praveen varma <npraveen.443@gmail.com>
2016-06-01 16:19:28 +0530

    Updated the TestReporter Utils class.

--- Desktop/eclipse/SeleniumProject/SampleProgram/src/Utils/TestReporter.java
+++ Desktop/eclipse/SeleniumProject/SampleProgram/src/Utils/TestReporter.java
@@ -3,7 +3,6 @@ package Utils;
 import java.io.File;
 import java.io.IOException;
 import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
 
 import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.OutputType;
