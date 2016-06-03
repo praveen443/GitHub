c745baf0c8f9a79db7fe0ba60d7c0061a6a68a54
praveen varma <npraveen.443@gmail.com>
2016-06-03 17:27:54 +0530

    Added a new test case - getTheCarsText() along with the new package name - toyota.

---
+++ Desktop/eclipse/SeleniumProject/SampleProgram/src/toyota/getTheCarsText.java
@@ -0,0 +1,21 @@
+package toyota;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class getTheCarsText {
+	private WebDriver driver;
+	
+	@Test(priority=1,enabled=true)
+	public void getCarNames(){
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.navigate().to("http://www.toyota.com/");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+	}
+
+}
