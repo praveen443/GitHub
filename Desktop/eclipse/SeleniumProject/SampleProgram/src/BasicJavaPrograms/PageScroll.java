bb170ddf1653223205368e9614625041d75042ee
praveen varma <npraveen.443@gmail.com>
2016-06-03 18:53:26 +0530

    Updated the test - PageScroll.java

--- Desktop/eclipse/SeleniumProject/SampleProgram/src/BasicJavaPrograms/PageScroll.java
+++ Desktop/eclipse/SeleniumProject/SampleProgram/src/BasicJavaPrograms/PageScroll.java
@@ -1,8 +1,11 @@
 package BasicJavaPrograms;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
@@ -20,17 +23,17 @@ public class PageScroll {
 	@Test(priority=1)
 	public void scrollingToBottomofAPage() {
 		driver.navigate().to(URL);
-		 JavascriptExecutor js = (JavascriptExecutor) driver;
-		 js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
 	}
 
-	/*@Test(priority=2)
+	@Test(priority=2)
 	public void scrollingToElementofAPage() {
 		driver.navigate().to(URL+"directory/companies?trk=hb_ft_companies_dir");
 		WebElement element = driver.findElement(By.linkText("Import/Export"));
 		((JavascriptExecutor) driver).executeScript(
                 "arguments[0].scrollIntoView();", element);
-	}*/
+	}
 	
 	@Test(priority=3)
 	public void scrollingByCoordinatesofAPage() {
@@ -38,8 +41,8 @@ public class PageScroll {
 		((JavascriptExecutor) driver).executeScript("window.scrollBy(0,500)");
 	}
 	
-	/*@AfterClass
+	@AfterClass
 	public void tearDown() {
 		driver.quit();
-	}*/
+	}
 }
