3fceb6c8c2c2ade868b8bdaae4603d6c777c193d
praveen443 <npraveen.443@gmail.com>
2016-06-06 17:36:22 +0530

    1.Added a new file - Constants.java 2.Added a new test data file - testDataFromPropertiesFile.properties. 3.Developed a new test - 'RedditLogin' using TestNG + Properties file.

---
+++ Desktop/eclipse/SeleniumProject/SampleProgram/src/testResources/ConstantInput/constants.java
@@ -0,0 +1,7 @@
+package testResources.ConstantInput;
+
+public class constants {
+
+	public static final String filePath = "C:\\Users\\praveen\\Desktop\\eclipse\\SeleniumProject\\SampleProgram\\src\\testResources\\testConfiguration\\testDataPropertiesFile.properties";
+
+}
