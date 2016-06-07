d91084d3f99125d055f68621b44861e68fa948e9
praveen443 <npraveen.443@gmail.com>
2016-06-07 15:19:56 +0530

    Updated the driver softwares.

--- Desktop/eclipse/SeleniumProject/SampleProgram/src/testResources/ConstantInput/constants.java
+++ Desktop/eclipse/SeleniumProject/SampleProgram/src/testResources/ConstantInput/constants.java
@@ -2,7 +2,12 @@ package testResources.ConstantInput;
 
 public class constants {
 
-	public static final String propertiesFilePath = System.getProperty("user.dir")+"\\src\\testResources\\testConfiguration\\testDataPropertiesFile.properties";
-    
+	//Driver paths
 	public static final String chromeDriverPath = System.getProperty("user.dir")+"\\drivers\\chromedriver.exe";
+	public static final String IE_32bit_DriverPath = System.getProperty("user.dir")+"\\drivers\\IEDriverServer_32_bit.exe";
+	public static final String IE_64bit_DriverPath = System.getProperty("user.dir")+"\\drivers\\IEDriverServer_64_bit.exe";
+
+	//Properties file path
+	public static final String propertiesFilePath = System.getProperty("user.dir")+"\\src\\testResources\\testConfiguration\\testDataPropertiesFile.properties";
+
 }
