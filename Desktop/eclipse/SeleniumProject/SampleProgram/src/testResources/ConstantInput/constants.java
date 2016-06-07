18a22e0c1bd97d37e073200a6b0e992b28ff044b
praveen443 <npraveen.443@gmail.com>
2016-06-07 13:48:53 +0530

    Added a new test classes to execute the reddit login functionality in different browsers.

--- Desktop/eclipse/SeleniumProject/SampleProgram/src/testResources/ConstantInput/constants.java
+++ Desktop/eclipse/SeleniumProject/SampleProgram/src/testResources/ConstantInput/constants.java
@@ -2,6 +2,6 @@ package testResources.ConstantInput;
 
 public class constants {
 
-	public static final String filePath = "C:\\Users\\praveen\\Desktop\\eclipse\\SeleniumProject\\SampleProgram\\src\\testResources\\testConfiguration\\testDataPropertiesFile.properties";
+	public static final String filePath = System.getProperty("user.dir")+"\\src\\testResources\\testConfiguration\\testDataPropertiesFile.properties";
 
 }
