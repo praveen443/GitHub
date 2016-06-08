77916a4098518c1e2d95ffcc9745afb76f6b8c92
praveen443 <npraveen.443@gmail.com>
2016-06-08 10:16:09 +0530

    Updated the following test classess - RedditLogin_UsingChrome & RedditLogin_UsingFirefox.

--- Desktop/eclipse/SeleniumProject/SampleProgram/src/readDataFromPropertiesFile/RedditLogin_UsingChrome.java
+++ Desktop/eclipse/SeleniumProject/SampleProgram/src/readDataFromPropertiesFile/RedditLogin_UsingChrome.java
@@ -13,7 +13,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 
 import testResources.ConstantInput.constants;
@@ -25,6 +24,7 @@ public class RedditLogin_UsingChrome {
   
 	private WebDriver driver;
 	public Properties prop = null;
+	@SuppressWarnings("unused")
 	private String testName;
 	HighlightElement ele = new HighlightElement();
 	
--- Desktop/eclipse/SeleniumProject/SampleProgram/src/readDataFromPropertiesFile/RedditLogin_UsingFirefox.java
+++ Desktop/eclipse/SeleniumProject/SampleProgram/src/readDataFromPropertiesFile/RedditLogin_UsingFirefox.java
@@ -13,7 +13,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import Utils.HighlightElement;
