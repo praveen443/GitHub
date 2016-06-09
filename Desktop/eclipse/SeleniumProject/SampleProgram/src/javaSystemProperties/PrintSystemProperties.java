690917cd860072a63511039a589b621e979d79be
praveen443 <npraveen.443@gmail.com>
2016-06-09 10:54:03 +0530

    Updated the test class - PrintSystemProperties.

--- Desktop/eclipse/SeleniumProject/SampleProgram/src/javaSystemProperties/PrintSystemProperties.java
+++ Desktop/eclipse/SeleniumProject/SampleProgram/src/javaSystemProperties/PrintSystemProperties.java
@@ -1,13 +1,23 @@
 package javaSystemProperties;
 
+import java.util.Properties;
+
 import org.testng.annotations.Test;
 
 public class PrintSystemProperties  {
   
   @Test
   public void printSystemProperties() {
-	 //prints java-version
+	
+	 //******************************************************************
+	 //JRE related system properties
+	 //****************************************************************** 
+	 //JRE version, e.g., 1.7.0_09.
 	 String java_version = System.getProperty("java.version");
+	 System.out.println("Java version ----> "+ java_version);
+	 
+	 //JRE home directory, e.g., “C:\Program Files\Java\jdk1.7.0_09\jre”.
+	 String java_home = System.getProperty("java.home");
 	 System.out.println("Java version ----> "+ java_version);
 	 
 	 //******************************************************************	 
@@ -25,14 +35,25 @@ public class PrintSystemProperties  {
 	 //******************************************************************
 	 // OS related system properties
 	 //******************************************************************
+	 //the OS’s version, e.g., “6.1”.
 	 String os_version = System.getProperty("os.version");
 	 System.out.println("Operating System version ----> "+os_version);
 	 
+	 //the OS’s name, e.g., “Windows 7”.
 	 String os_Name = System.getProperty("os.name");
 	 System.out.println("Operating System name ----> "+os_Name);
 	 
+	 //the OS’s architecture, e.g., “x86”.
 	 String os_architecture = System.getProperty("os.arch");
 	 System.out.println("OS Architecture ----> "+os_architecture);
+	 
+	 //******************************************************************
+	 // List all System properties
+	 //******************************************************************
+	 Properties pros = System.getProperties();
+     pros.list(System.out);
+     
+     //******************************************************************
   }
   
 }
