18d739a66187f242c51b3406efca4758434ec7ab
praveen443 <npraveen.443@gmail.com>
2016-06-09 10:23:01 +0530

    Updated the test class - PrintSystemProperties.java

--- Desktop/eclipse/SeleniumProject/SampleProgram/src/javaSystemProperties/PrintSystemProperties.java
+++ Desktop/eclipse/SeleniumProject/SampleProgram/src/javaSystemProperties/PrintSystemProperties.java
@@ -9,8 +9,10 @@ public class PrintSystemProperties  {
 	 //prints java-version
 	 String java_version = System.getProperty("java.version");
 	 System.out.println("Java version ----> "+ java_version);
-		 
-	 //prints the project base directory
+	 
+	 //******************************************************************	 
+	 //User related system properties
+	 //******************************************************************
 	 String baseDirectory = System.getProperty("user.dir");
 	 System.out.println("Base-Directory/path ----> "+ baseDirectory);
 	 
@@ -20,7 +22,17 @@ public class PrintSystemProperties  {
 	 String user_home = System.getProperty("user.home");
 	 System.out.println("User directory path ----> "+ user_home);
 	 
+	 //******************************************************************
+	 // OS related system properties
+	 //******************************************************************
+	 String os_version = System.getProperty("os.version");
+	 System.out.println("Operating System version ----> "+os_version);
 	 
+	 String os_Name = System.getProperty("os.name");
+	 System.out.println("Operating System name ----> "+os_Name);
+	 
+	 String os_architecture = System.getProperty("os.arch");
+	 System.out.println("OS Architecture ----> "+os_architecture);
   }
   
 }
