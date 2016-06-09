32389d2ce47f728b5ef69519d1f9682044cacabb
praveen443 <npraveen.443@gmail.com>
2016-06-09 18:04:00 +0530

    Updated the Sleeper.utils class

--- Desktop/eclipse/SeleniumProject/SampleProgram/src/Utils/Sleeper.java
+++ Desktop/eclipse/SeleniumProject/SampleProgram/src/Utils/Sleeper.java
@@ -1,12 +1,21 @@
 package Utils;
 
+import org.apache.commons.lang.time.StopWatch;
+
 public class Sleeper {
 		
-	public static void sleep(long milliSeconds){
-			try {
-				Thread.sleep(milliSeconds);
-			} catch (InterruptedException e) {
-				
-			}
+	/**
+	* This method will pause test execution for an amount of time defined by the user
+	* @param millis - amount of time for which to wait, in milliseconds
+	*/
+	public static void sleep(long milliseconds){
+		StopWatch stopwatch = new StopWatch();
+		stopwatch.start();
+		do {
+			
+		} while (stopwatch.getTime() < milliseconds);
+		stopwatch.stop();
+		stopwatch.reset();
 	}
+
 }
