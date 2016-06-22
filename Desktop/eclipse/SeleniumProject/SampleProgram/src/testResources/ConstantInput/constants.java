dc3424bf57f3bb8738a90cfa4293c2cc26166da2
praveen443 <npraveen.443@gmail.com>
2016-06-22 13:12:35 +0530

    updated constant.class

--- Desktop/eclipse/SeleniumProject/SampleProgram/src/testResources/ConstantInput/constants.java
+++ Desktop/eclipse/SeleniumProject/SampleProgram/src/testResources/ConstantInput/constants.java
@@ -2,6 +2,7 @@ package testResources.ConstantInput;
 
 public class constants {
 
+	
 	//Driver paths
 	public static final String chromeDriverPath = System.getProperty("user.dir")+"\\drivers\\chromedriver.exe";
 	public static final String IE_32bit_DriverPath = System.getProperty("user.dir")+"\\drivers\\IEDriverServer_32_bit.exe";
