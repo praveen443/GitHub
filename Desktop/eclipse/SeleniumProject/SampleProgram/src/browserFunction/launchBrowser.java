8dbd26a18daa0c0801df0cd02186309de53fab45
praveen443 <npraveen.443@gmail.com>
2016-06-22 14:51:14 +0530

    Updated the pom.xml file.

--- Desktop/eclipse/SeleniumProject/SampleProgram/src/browserFunction/launchBrowser.java
+++ Desktop/eclipse/SeleniumProject/SampleProgram/src/browserFunction/launchBrowser.java
@@ -16,6 +16,7 @@ import testResources.ConstantInput.constants;
 import Utils.TestReporter;
 
 public class launchBrowser {
+	@SuppressWarnings("unused")
 	private WebDriver driver;
 	private static String testName;
 	//Constructor to initialize the page.
